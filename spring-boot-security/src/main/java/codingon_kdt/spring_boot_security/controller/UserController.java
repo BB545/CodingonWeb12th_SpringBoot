9c132fdc909be3075504636b442b449b33672ea8
BB545 <black1016@hanmail.net>
2024-07-23 10:52:59 +0900

    study: SpringBoot user 테이블 생성 및 회원가입, 로그인 연습

---
+++ spring-boot-security/src/main/java/codingon_kdt/spring_boot_security/controller/UserController.java
@@ -0,0 +1,75 @@
+package codingon_kdt.spring_boot_security.controller;
+
+import codingon_kdt.spring_boot_security.domain.UserEntity;
+import codingon_kdt.spring_boot_security.dto.ResponseDTO;
+import codingon_kdt.spring_boot_security.dto.UserDTO;
+import codingon_kdt.spring_boot_security.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/auth") // 기본 매핑 경로
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/signup")
+    // ResponseEntity: 사용자에게 좀 더 응답을 편하게 해주고자 사용
+    public ResponseEntity<?> registerUser(@RequestBody UserDTO userDTO) {
+        // 요청으로 받은 DTO 를 userEntity 로 변환 - 다른 계층 타입 일치 위해
+        try {
+            UserEntity user = UserEntity.builder()
+                    .email(userDTO.getEmail())
+                    .username(userDTO.getUsername())
+                    .password(userDTO.getPassword())
+                    .build();
+
+            // 서비스 계층을 이용해 레포지토리에 저장
+            UserEntity registeredUser = userService.create(user);
+
+            // 저장된 사용자 정보로 UserDTO 를 생성해 응답 본문에 담아 리턴
+            // password 는 보안상 일부러 보내지 않음
+            UserDTO responseUserDTO = UserDTO.builder()
+                    .email(registeredUser.getEmail())
+                    .id(registeredUser.getId())
+                    .username(registeredUser.getUsername())
+                    .build();
+
+            return ResponseEntity.ok().body(responseUserDTO);
+        } catch (Exception e) {
+            // 사용자 정보는 항상 하나이므로 성공 데이터에 대해 리스트로 만들어야 하는 responseDTO 를 사용하지 않고 그냥 userDTO 를 반환
+            // 예외 상황에서는 error 메세지를 보여주기 위해 responseDTO 를 사용함
+            ResponseDTO responseDTO = ResponseDTO.builder().error(e.getMessage()).build();
+
+            return ResponseEntity.badRequest().body(responseDTO);
+        }
+    }
+
+    @PostMapping("signin")
+    public ResponseEntity<?> authenticate(@RequestBody UserDTO userDTO) {
+        // UserService 계층을 통해 이메일과 비밀번호로 사용자를 조회
+        UserEntity user = userService.getByCredentials(userDTO.getEmail(), userDTO.getPassword());
+
+        if (user != null) {
+            final UserDTO responseUserDTO = UserDTO.builder()
+                    .email(user.getEmail())
+                    .id(user.getId())
+                    .build();
+
+            return ResponseEntity.ok().body(responseUserDTO);
+        } else {
+            ResponseDTO responseDTO = ResponseDTO.builder()
+                    .error("Login failed")
+                    .build();
+
+            // ResponseDTO 에 정의해둔 error 메세지 이용해서 표시
+            return ResponseEntity.badRequest().body(responseDTO);
+        }
+    }
+}
